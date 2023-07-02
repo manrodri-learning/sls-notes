5dbe5fa27597d9b81658cce924406c440c0358d5
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2023-07-02 11:15:47 +0100

    update api from course file

--- API/update-note.js
+++ API/update-note.js
@@ -1,47 +1,49 @@
-// Rote note: PATCH /note/::id
-const AWS = require("aws-sdk");
-const util = require("./utils.js");
-AWS.config.update({ region: "eu-west-1" });
+/**
+ * Route: PATCH /note
+ */
 
-const dynamodb = new AWS.DynamoDB.DocumentClient();
-const tableName = process.env.NOTES_TABLE;
-
-exports.handler = async (event) => {
-  try {
-    let item = JSON.parse(event.body).Item;
-    item.user_id = util.getUserId(event.headers);
-    item.user_name = util.getUserName(event.headers);
-    item.expires = moment().add(90, "days").unix();
-
-    let data = awaitdynamodb
-      .put({
-        TableName: tableName,
-        Item: item,
-        ConditionExpression: "#t = :t",
-        ExpressionAttributeNames: {
-          "#t": "timestamp"
-        },
-        ExpressionAttributeValues: {
-          ":t": item.timestamp
-        }
-      })
-      .promise();
-
-    return {
-      statusCode: 200,
-      headers: util.getResponseHeaders(),
-      body: JSON.stringify(item)
-    };
-  } catch (err) {
-    console.log("Error", err);
-
-    return {
-      statusCode: err.statusCode ? err.statusCode : 500,
-      body: JSON.stringify({
-        error: err.name ? err.name : "Exception",
-        message: err.message ? err.message : "Unknown error"
-      }),
-      headers: util.getResponseHeaders()
-    };
-  }
-};
+ const AWS = require('aws-sdk');
+ AWS.config.update({ region: 'us-west-2' });
+ 
+ const moment = require('moment');
+ const util = require('./util.js');
+ 
+ const dynamodb = new AWS.DynamoDB.DocumentClient();
+ const tableName = process.env.NOTES_TABLE;
+ 
+ exports.handler = async (event) => {
+     try {
+         let item = JSON.parse(event.body).Item;
+         item.user_id = util.getUserId(event.headers);
+         item.user_name = util.getUserName(event.headers);
+         item.expires = moment().add(90, 'days').unix();
+ 
+         let data = await dynamodb.put({
+             TableName: tableName,
+             Item: item,
+             ConditionExpression: '#t = :t',
+             ExpressionAttributeNames: {
+                 '#t': 'timestamp'
+             },
+             ExpressionAttributeValues: {
+                 ':t': item.timestamp
+             }
+         }).promise();
+ 
+         return {
+             statusCode: 200,
+             headers: util.getResponseHeaders(),
+             body: JSON.stringify(item)
+         };
+     } catch (err) {
+         console.log("Error", err);
+         return {
+             statusCode: err.statusCode ? err.statusCode : 500,
+             headers: util.getResponseHeaders(),
+             body: JSON.stringify({
+                 error: err.name ? err.name : "Exception",
+                 message: err.message ? err.message : "Unknown error"
+             })
+         };
+     }
+ }
